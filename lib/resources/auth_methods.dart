be8d34d673afcb8cab861be380325fec9b3892a7
Ali Raza <ialidar2001@gmail.com>
2023-02-18 01:35:12 +0500

    Added backend file

---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //sign up user
+
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    // required Uint8List file,
+  }) async {
+    String res = "Some error occured";
+
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty) {
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        // adding user to firestore
+
+        _firestore.collection('users').doc(cred.user!.uid).set({
+          'email': email,
+          'username': username,
+          'bio': bio,
+          'uid': cred.user!.uid,
+          'followers': [],
+          'following': [],
+        });
+
+        res = 'success';
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_flutter/resources/auth_methods.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 import 'package:instagram_flutter/widgets/text_field_input.dart';
 
@@ -145,7 +146,13 @@ class _SignupScreenState extends State<SignupScreen> {
                     child: const Text("Don't have an account?"),
                   ),
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+                      AuthMethods().signUpUser(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                          username: _usernameController.text,
+                          bio: _bioController.text);
+                    },
                     child: Container(
                       padding: const EdgeInsets.symmetric(
                         vertical: 8,
